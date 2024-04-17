35077317ac4f63091faef31748c560a5ec44992f
javiergs <3814755+javiergs@users.noreply.github.com>
2024-04-17 02:14:05 -0700

    version 2

---
+++ version-02/src/MapPanel.java
@@ -0,0 +1,43 @@
+import javax.swing.*;
+import java.awt.*;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URL;
+import java.net.URLConnection;
+
+public class MapPanel extends JPanel {
+
+  public MapPanel() {
+    setBackground(Color.LIGHT_GRAY);
+    try {
+      ImageIcon imageIcon = getMap();
+      imageIcon.getImage().getScaledInstance(200, 200, Image.SCALE_SMOOTH);
+      add(new JLabel(imageIcon));
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+  private ImageIcon getMap() throws Exception {
+    String location = "35.270378,-120.680656";
+    String apiKey = "AiSeFXl4iTK4QhhhkPTfUOulN5_rFaTBC62yquebCDL9anZNIP333urvmwXnuFz6";
+    String mapUrl = "https://dev.virtualearth.net/REST/v1/Imagery/Map/Road/"
+      + location + "?zoomLevel=10&mapSize=200,200&key="
+      + apiKey;
+
+    URL mapImageUrl = new URL(mapUrl);
+    URLConnection connection = mapImageUrl.openConnection();
+    InputStream inputStream = connection.getInputStream();
+
+    OutputStream outputStream = new FileOutputStream("image.jpg");
+    byte[] b = new byte[2048];
+    int length;
+    while ((length = inputStream.read(b)) != -1) {
+      outputStream.write(b, 0, length);
+    }
+    inputStream.close();
+    outputStream.close();
+    return new ImageIcon("image.jpg");
+  }
+}
